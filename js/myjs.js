407015431e3ea756bc2453c790281e02d8e4920b
Mahito Sugiyama <mahito.sugiyama@gmail.com>
2017-04-10 14:42:49 +0900

    update info

--- js/myjs.js
+++ js/myjs.js
@@ -1,13 +1,34 @@
+var isVisible_r = false;
+var clickedAway_r = false;
+
 var isVisible_j = false;
 var clickedAway_j = false;
 
 var isVisible_c = false;
 var clickedAway_c = false;
 
+$('.popr').each(function() {
+	var $pElem = $(this);
+
+	$pElem.popover({
+		html: true,
+		placement: 'bottom',
+		title: getPopTitle($pElem.attr('id')),
+		content: getPopContent($pElem.attr('id'))
+	}).click(function(e) {
+		if (!isVisible_r) {
+			$(this).popover('show');
+			isVisible_r = true
+			clickedAway_r = false
+			e.preventDefault();
+		}
+	})
+});
+
 $('.popj').each(function() {
 	var $pElem = $(this);
 
-	$pElem.popover({ 
+	$pElem.popover({
 		html: true,
 		placement: 'bottom',
 		title: getPopTitle($pElem.attr('id')),
@@ -25,7 +46,7 @@ $('.popj').each(function() {
 $('.popc').each(function() {
 	var $pElem = $(this);
 
-	$pElem.popover({ 
+	$pElem.popover({
 		html: true,
 		placement: 'bottom',
 		title: getPopTitle($pElem.attr('id')),
@@ -41,22 +62,30 @@ $('.popc').each(function() {
 });
 
 $(document).click(function(e) {
-	if(isVisible_j & clickedAway_j) {
-		if (e.target.className != "popover-content" & e.target.className != "popover-title") {
-			$('.popj').popover('hide');
-			isVisible_j = clickedAway_j = false
-		}
-	} else {		
-		clickedAway_j = true
+    if(isVisible_r & clickedAway_r) {
+	if (e.target.className != "popover-content" & e.target.className != "popover-title") {
+	    $('.popr').popover('hide');
+	    isVisible_r = clickedAway_r = false
 	}
-	if(isVisible_c & clickedAway_c) {
-		if (e.target.className != "popover-content" & e.target.className != "popover-title") {
-			$('.popc').popover('hide');
-			isVisible_c = clickedAway_c = false
-		}
-	} else {		
-		clickedAway_c = true
+    } else {		
+	clickedAway_r = true
+    }
+    if(isVisible_j & clickedAway_j) {
+	if (e.target.className != "popover-content" & e.target.className != "popover-title") {
+	    $('.popj').popover('hide');
+	    isVisible_j = clickedAway_j = false
 	}
+    } else {		
+	clickedAway_j = true
+    }
+    if(isVisible_c & clickedAway_c) {
+	if (e.target.className != "popover-content" & e.target.className != "popover-title") {
+	    $('.popc').popover('hide');
+	    isVisible_c = clickedAway_c = false
+	}
+    } else {		
+	clickedAway_c = true
+    }
 });
 
 
